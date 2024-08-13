ff7623a6ad6e9580b8625c6e4ef3f94fd5e47253
Belinda Lai <b.emma.lai@gmail.com>
2024-08-13 13:11:40 -0500

    Bypass cert validation

--- runshoutbombholds.bat
+++ runshoutbombholds.bat
@@ -1,7 +1,7 @@
 REM Run the sqlworkbench scripts for holds only. Scripts also save files.
 java -jar C:\SQLWorkbench\sqlworkbench.jar -script='c:/shoutbomb/scripts/holds.sql' -profile=CountyCat
 REM Run winscp to ftp the files over to Shoutbomb
-winscp.com /command "option batch abort" "option confirm off" "open shoutbomb" "put C:\Shoutbomb\Holds\*.* /Holds/" "exit"
+winscp.com /command "option batch abort" "option confirm off" "open shoutbomb -certificate=*" "put C:\Shoutbomb\Holds\*.* /Holds/" "exit"
 REM move the ftp'd files over to ftpsent folder
 CD C:\
 MOVE C:\Shoutbomb\Holds\*.* C:\Shoutbomb\Archive\Holds
