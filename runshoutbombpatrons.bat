b545b9d6ee56d90fc21f485cd60d34d84f81ba21
Belinda Lai <b.emma.lai@gmail.com>
2024-10-23 13:14:50 -0500

    Add batch file for sending patrons signed up for text notices via FTP

---
+++ runshoutbombpatrons.bat
@@ -0,0 +1,9 @@
+REM Run the sqlworkbench scripts for text patrons. Scripts also save files.
+java -jar C:\SQLWorkbench\sqlworkbench.jar -script='c:/shoutbomb/scripts/text.sql' -profile=CountyCat
+REM Run winscp to ftp the files over to Shoutbomb
+winscp.com /command "option batch abort" "option confirm off" "open shoutbomb -certificate=*" "put C:\shoutbomb\Text\*.* /text_patrons/" "exit"
+REM move the ftp'd files over to ftpsent folder
+CD C:\
+MOVE C:\Shoutbomb\Text\*.* C:\Shoutbomb\Archive\Text
+REM Delete archive files older than 30 days
+forfiles -p "C:\shoutbomb\Archive\Text" -s -m *.* /D -30 /C "cmd /c del @path"
